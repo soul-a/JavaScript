df796be70e0eddec6281ba2e7bc61922ff38882d
soul-a <ryan1858.rs@hotmail.com>
2021-07-21 20:59:12 -0300

    Entendendo o Console e suas utilidades

---
+++ console.js
@@ -0,0 +1,12 @@
+// Console
+
+const minhaVar = true;
+
+console.log("eu sou um texto")
+console.log(123)
+console.log(minhaVar)
+
+// Tratamento de erros
+
+console.error(new Error("Houve um erro durante a execução!"))
+
