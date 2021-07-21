db18859ae0f8aaee3f3cf550258c9a68398b97cf
soul-a <ryan1858.rs@hotmail.com>
2021-07-21 20:30:00 -0300

    Erros e stacktrace

---
+++ erros.js
@@ -0,0 +1,25 @@
+
+/**
+ * SyntaxError: Missing initializer
+ * in const declaration
+ * 
+ * const não podem ser criadas sem valores
+ */
+//const numero;
+
+/**
+ * SyntaxError: missing ) after argument list
+ * 
+ * Faltou o fechar parenteses
+ */
+//const numero = 1;
+
+//console.log(numero
+
+/**
+ * ReferenceError: minhaVar is not defined
+ * 
+ * Erro de referência dizendo que a variável chamada não foi declarada
+ */
+console.log(minhaVar);
+
