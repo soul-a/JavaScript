198aee27363a3df8810f3d1ca5cb6c06c8abca9e
soul-a <ryan1858.rs@hotmail.com>
2022-04-04 21:23:04 -0300

    Utilizando o for

---
+++ js-arrays/for.js
@@ -0,0 +1,12 @@
+const numeros = [100, 200, 300, 400, 500, 600]
+
+// Maneira com for
+// for (let i = 0; i < numeros.length; i++) {
+//     console.log(`${i} -> ${numeros[i]}`)
+// }
+
+// Maneira com foreach
+numeros.forEach(numero => {
+    console.log(numero)
+})
+
