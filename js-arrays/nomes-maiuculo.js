7e907cbd4775d7a2890c8fc892afe774a2199b80
soul-a <ryan1858.rs@hotmail.com>
2022-04-05 21:30:45 -0300

    Usando o map em arrays

---
+++ js-arrays/nomes-maiuculo.js
@@ -0,0 +1,8 @@
+const nomes = ['ana Julia', 'Caio vinicius', 'BIA silva']
+
+const nomesAtualizadoMaiusculo = nomes.map(nome => {
+    return nome.toUpperCase()
+})
+
+console.log(nomes)
+console.log(nomesAtualizadoMaiusculo)
---
+++ js-arrays/ponto-extra.js
@@ -0,0 +1,12 @@
+const notas = [10, 9, 8, 7, 6]
+
+// const notasAtualizadas = notas.map(nota => {
+//     if(nota == 10)
+//         return nota
+//     else
+//         return ++nota
+// })
+
+const notasAtualizadas = notas.map(nota => nota == 10 ? nota : ++nota)
+
+console.log(notasAtualizadas)
