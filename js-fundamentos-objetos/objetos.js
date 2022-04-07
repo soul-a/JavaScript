02e4d11df5c8e11e13e98c062d1d4fc6625aa289
soul-a <ryan1858.rs@hotmail.com>
2022-04-06 22:00:46 -0300

    Diferença entre objetos e arrays/variáveis primitivas

---
+++ js-fundamentos-objetos/objetos.js
@@ -0,0 +1,11 @@
+const listaCpfs = ["312321321", "31234214124", "5523543232"]
+
+const cliente = {
+    nome: "André",
+    idade: 34,
+    cpf: "321321321",
+    email: "teste@email.com"
+}
+
+console.log(listaCpfs)
+console.log(cliente)
