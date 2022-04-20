5045677c937ea76534f264e31e03acd0735883dc
soul-a <ryan1858.rs@hotmail.com>
2022-04-19 21:28:22 -0300

    Inserindo e alterando atributos dentro de um objeto

---
+++ js-fundamentos-objetos/adicionar-campo.js
@@ -0,0 +1,16 @@
+const cliente = {
+    nome: "André",
+    idade: 34,
+    cpf: "321321321",
+    email: "teste@email.com"
+}
+
+console.log(cliente)
+
+cliente.fone = "17 996608685"
+
+console.log(cliente)
+
+cliente.fone = "5517996608685"
+
+console.log(cliente)
